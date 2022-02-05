7e8043c8813978bc8c8183bfe9ef8a4519cd9e25
IBXCODECAT <nathan.schmitt7@gmail.com>
2022-02-04 22:24:28 -0600

    Add Perspective Webhook

--- src/index.js
+++ src/index.js
@@ -2,8 +2,38 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-//Discord modules
+//Import Google Modules (Perspective Comment API)
+import perspective from 'googleapis'
+
+//Import Discord modules
 import DiscordJS, { Intents } from 'discord.js';
+
+
+perspective.discoverAPI(process.env.DISCOVERY_URL)
+    .then(google => {
+        const analyzeRequest = {
+            comment: {
+                text: 'Jiminy cricket! Well gosh durned it! Oh damn it all!',
+            },
+            requestedAttributes: {
+                TOXICITY: {},
+            },
+        };
+
+        google.comments.analyze(
+            {
+                key: process.env.PERSPECTIVE_API_KEY,
+                resource: analyzeRequest,
+            },
+            (err, response) => {
+                if (err) throw err;
+                console.log(JSON.stringify(response.data, null, 2));
+            });
+    })
+    .catch(err => {
+        throw err;
+});
+
 
 const client = new DiscordJS.Client({
     intents: [ //Tells discord what kind of information you need sent
@@ -12,7 +42,7 @@ const client = new DiscordJS.Client({
     ]
 });
 
-client.login(process.env.API_TOKEN); //Login using discord secure token
+client.login(process.env.DISCORD_API_KEY); //Login using discord secure token
 
 client.on("ready", async () => {
     console.log("The bot is online according to node...");
